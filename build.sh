86f0a99983a24a3909da21ae91f145b280936d97
Anton Indrawan <anton.indrawan@gmail.com>
2015-10-25 21:24:18 +0100

    Try to build socketcand using android cmake. Issues on pthread_cancel

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+target_os=linux
+
+if [[ -z ${1-} ]]; then
+  echo "using linux as default"
+elif [[ ${1} == "android" ]]; then
+  target_os=android
+fi
+
+readonly target_os
+readonly script_dir=$(cd $(dirname $BASH_SOURCE[0]); pwd)
+readonly source_dir=${script_dir}
+readonly output_dir=${script_dir}/output/${target_os}
+
+
+cmake_extra_args="-DLINUX=1"
+if [[ ${target_os} == "android" ]]; then
+  cmake_extra_args="-DCMAKE_TOOLCHAIN_FILE=${source_dir}/android.toolchain.cmake \
+                    -DANDROID_NDK=${ANDROID_NDK} \
+                    -DANDROID_NATIVE_API_LEVEL=android-21 \
+                    -DANDROID=1
+                   "
+fi
+
+mkdir -p ${output_dir}
+cd ${output_dir}
+cmake ${cmake_extra_args} ${source_dir}
+make -j4
